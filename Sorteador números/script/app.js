b136da7e12e3a6d4bcdfaf7f7fcba1373dcb54d9
Lezin <lazim.franciscoo@gmail.com>
2025-02-18 19:11:10 -0300

    refactor: corrigido o sorteio de números e adicionado controle de estado do botão

--- Sorteador números/script/app.js
+++ Sorteador números/script/app.js
@@ -1,9 +1,10 @@
 // Função que realiza o sorteio de números
 function sortear() {
-        let quantidade = parseInt(document.getElementById('quantidade').value);
-        let de = document.getElementById('de').value;
-    
-        alert(`Sorteando ${quantidade} números entre ${de} e ${ate}`);
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    alert(`Sorteando ${quantidade} números entre ${de} e ${ate}`);
 
     // Array para armazenar os números sorteados
     let numerosSorteados = [];
@@ -17,6 +18,28 @@ function sortear() {
         numerosSorteados.push(numeroSorteado);
     }
 
-        let resultado = document.getElementById('resultado');
-        resultado.innerHTML = numerosSorteados.join(', ');
-} 
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = numerosSorteados.join(', ');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+        alterarStatusBotao();
+}
+
+// Função que altera o status do botão
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+            botao.classList.remove('container__botao-desabilitado');
+            botao.classList.add('container__botao');
+    } else {
+            botao.classList.remove('container__botao');
+            botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarStatusBotao();
+}
